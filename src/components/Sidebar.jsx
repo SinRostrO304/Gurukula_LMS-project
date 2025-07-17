0a6f47ce40798f8e595fe757fc6a32c0baa3f8c1
SinRostrO304 <lahirumweerasekara@gmail.com>
2025-07-17 19:26:07 +0530

    Updated files after local edits

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,6 @@ import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { useTheme } from '@mui/material/styles';
 import { useContext } from 'react';
 import { ColorModeContext } from '../shared-theme/AppTheme';
-import { ColorModeContext } from '../shared-theme/AppTheme';
 
 export default function Sidebar({
   activeSection,
